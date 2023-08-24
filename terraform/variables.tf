0621d074af47c86bdd5460ad74c099df4f923943
Mihail Marian <mihailmarian12@gmail.com>
2023-08-24 07:23:13 +0100

    wip

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,10 @@
 variable "resource_tags" {
   type = map(string)
   default = {
-    Name        = "fight_alerts_resource"
-    Owner       = "Mihail_Marian"
-    Contact     = "mihail@email.com"
-    Environment = var.environment
-    Product     = "Fight_Alerts"
+    Name    = "fight_alerts_resource"
+    Owner   = "Mihail_Marian"
+    Contact = "mihail@email.com"
+    Product = "Fight_Alerts"
   }
 }
 
