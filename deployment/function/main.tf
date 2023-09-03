62d88fc0f84549e0ddbb1e5af2af69b889e5ff9f
Mihail Marian <mihailmarian12@gmail.com>
2023-09-03 11:25:56 +0100

    wip

--- deployment/function/main.tf
+++ deployment/function/main.tf
@@ -1,12 +1,13 @@
 locals {
-  db_username = data.aws_ssm_parameter.db_username.value
-  db_password = data.aws_ssm_parameter.db_password.value
+  db = {
+    username = data.aws_ssm_parameter.db_username.value
+    password = data.aws_ssm_parameter.db_password.value
+  }
 }
 
 module "lambda" {
   source      = "./modules/lambda"
   environment = var.environment
   region      = var.region
-  db_username = local.db_username
-  db_password = local.db_password
+  db          = local.db
 }
