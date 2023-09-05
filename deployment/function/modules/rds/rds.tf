678b0d61ad272a1765988a35f50431f46c6f73b4
mmarian <mihailmarian12@gmail.com>
2023-09-05 22:56:00 +0100

    wip

--- deployment/function/modules/rds/rds.tf
+++ deployment/function/modules/rds/rds.tf
@@ -18,6 +18,5 @@ resource "aws_rds_cluster_instance" "single_instance" {
   engine                 = aws_rds_cluster.rds_cluster.engine
   instance_class         = "db.t3.medium"
   engine_version         = aws_rds_cluster.rds_cluster.engine_version
-  ca_cert_identifier     = "rds-ca-ecc384-g1"
   publicly_accessible    = true
 }
